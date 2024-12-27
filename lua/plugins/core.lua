52f8d835182ff15dc3b95db8b9c705d8e52c84e0
misilelab <misileminecord@gmail.com>
2024-12-27 11:12:34 +0900

    feat: init lazyvim
    Signed-off-by: misilelab <misileminecord@gmail.com>

---
+++ lua/plugins/core.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    { "LazyVim/LazyVim", version = false },
+    { "folke/lazy.nvim", version = false },
+    { "wakatime/vim-wakatime", lazy = false },
+    opts = {
+      colorscheme = "catppuccin"
+    }
+  }
+}
