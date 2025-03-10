95c0bf66366f3fb2fac73e8d70c0d06d4c2792ae
misilelab <misileminecord@gmail.com>
2025-03-10 13:09:14 +0900

    feat: add co-author.nvim
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -9,5 +9,20 @@ return {
     },
     { "folke/lazy.nvim",       version = false },
     { "wakatime/vim-wakatime", lazy = false },
+    {
+      "2kabhishek/nerdy.nvim",
+      dependencies = {
+        "nvim-telescope/telescope.nvim"
+      },
+      cmd = "Nerdy"
+    },
+    { "neovim/nvim-lspconfig",
+      opts = {
+        diagnostics = {
+          update_in_insert = true
+        }
+      }
+    },
+    {"2kabhishek/co-author.nvim"}
   }
 }
