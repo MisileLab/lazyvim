17482339a8024e6b8aaffcc49954366ede106ac7
misilelab <misileminecord@gmail.com>
2025-03-13 00:11:07 +0900

    chore: unpin dependencies
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -10,21 +10,23 @@ return {
       }
     },
     { "folke/lazy.nvim",       version = false },
-    { "wakatime/vim-wakatime", lazy = false },
+    { "wakatime/vim-wakatime", lazy = false, version = false },
     {
       "2kabhishek/nerdy.nvim",
       dependencies = {
         "nvim-telescope/telescope.nvim"
       },
-      cmd = "Nerdy"
+      cmd = "Nerdy",
+      version = false
     },
     { "neovim/nvim-lspconfig",
       opts = {
         diagnostics = {
           update_in_insert = true
         }
-      }
+      },
+      version = false
     },
-    {"2kabhishek/co-author.nvim"}
+    {"2kabhishek/co-author.nvim", version = false}
   }
 }
