577155efbd37b2520ae6168338a11137510cd3b9
misilelab <misileminecord@gmail.com>
2025-04-07 10:38:00 +0900

    fix: some lualine
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,5 @@
 return {
   {"williamboman/mason.nvim", enabled = false},
-  {"williamboman/mason-lspconfig.nvim", enabled = false}
+  {"williamboman/mason-lspconfig.nvim", enabled = false},
+  {"jay-babu/mason-nvim-dap.nvim", enabled = false}
 }
